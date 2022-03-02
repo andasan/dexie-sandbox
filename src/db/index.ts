7a348a5664f6760b897d10fcdc541bf04ace4064
Francois Polo <andasan@gmail.com>
2022-03-01 19:14:05 -0800

    :card_file_box: add to db function creation

--- src/db/index.ts
+++ src/db/index.ts
@@ -23,6 +23,11 @@ export const getAllRowsByField = (field: string, filter: number, sortBy: string)
 export const countByFieldAndFilter = (field:string, filter:number) => {
   return db.todos.where(field).equals(filter).count()
 }
+
+export const add = (data: Todo) => {
+  db.todos.add(data);
+}
+
 export const modifyField = (id: number, field:string) => {
   db.todos.where({id: id}).modify((x) => ++x[field as keyof Todo])
 }
